96106b12ee8cf9fd9a4a317695a4812c97114320
Alexandra06T <email.alexandra.toma@gmail.com>
2024-11-27 20:11:50 +0200

    added unit tests for addContent from contentManager

--- contracts/ContentManager.sol
+++ contracts/ContentManager.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "hardhat/console.sol";
 
 contract ContentManager is Ownable{
 
@@ -12,11 +13,6 @@ contract ContentManager is Ownable{
         require (msg.sender == creator, "Action allowed only for the creator of the content!");
         _;
     } 
-
-    modifier OnlyCreatorOrAdmin (address creator){
-        require (msg.sender == creator, "Action allowed only for the admin or the creator of the content!");
-        _;
-    }
 
     modifier OnlyExistentContent (string memory CID) {
         require (contents[CID].creator != address(0), "Content not found!");
@@ -50,19 +46,18 @@ contract ContentManager is Ownable{
     function addContent(uint price, string memory CID) public {
         
         require (contents[CID].creator == address(0), "Content is already on the platform!");
-
+        
         contents[CID] = Content({
             creator: msg.sender,
             price: price,
             usageCount: 0,
             CID: CID
         });
-
         
         emit ContentAdded(msg.sender, CID);
     }
 
-    function removeContent(string memory CID) public OnlyCreatorOrAdmin(contents[CID].creator) OnlyExistentContent(CID) {
+    function removeContent(string memory CID) public OnlyCreator(contents[CID].creator) OnlyExistentContent(CID) {
 
        delete contents[CID]; 
 
---
+++ test/contentManager.test.js
@@ -0,0 +1,66 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { expect } = require('chai');
+
+describe('ContentManager', function () {
+
+  async function deployContractAndSetVariables() {
+    const [owner, user1, user2] = await ethers.getSigners();
+
+    const ContentManager = await ethers.getContractFactory('ContentManager');
+    const contentManager = await ContentManager.deploy(owner);
+
+    let cid = 'bafkreibkfkqs5iax34mccrllg352slzz7x4nwkftpxe5yt5qd236gsxz4i';
+
+    return { contentManager, owner, user1, cid};
+  }
+
+  it('should deploy and set the owner correctly', async function () {
+    const { contentManager, owner } = await loadFixture(deployContractAndSetVariables);
+
+    expect(await contentManager.owner()).to.equal(owner.address);
+  });
+
+  it('should get the content with given CID correctly', async function () {
+    const { contentManager, owner, user1, cid } = await loadFixture(deployContractAndSetVariables);
+
+    await contentManager.addContent(10, cid);
+
+    gotContent = await contentManager.getContent(cid);
+
+    expect(gotContent.creator).to.equal(owner.address);
+    expect(gotContent.price).to.equal(10);
+    expect(gotContent.usageCount).to.equal(0);
+  });
+
+  it('should save the content', async function () {
+    const { contentManager, owner, user1, cid } = await loadFixture(deployContractAndSetVariables);
+
+    await contentManager.addContent(10, cid);
+
+    const savedContent = await contentManager.getContent(cid);
+
+    expect(savedContent).to.not.equal(undefined);
+    expect(savedContent.creator).to.equal(owner.address);
+    expect(savedContent.price).to.equal(10);
+    expect(savedContent.usageCount).to.equal(0);
+  });
+
+  it('should emit ContentAdded event after saving the content', async function () {
+    const { contentManager, owner, user1, cid } = await loadFixture(deployContractAndSetVariables);
+
+    await expect(contentManager.addContent(10, cid)).to.emit(contentManager, "ContentAdded").withArgs(owner, cid);
+  });
+
+  it('should not allow uploads of duplicate content', async function () {
+    const { contentManager, owner, cid } = await loadFixture(deployContractAndSetVariables);
+
+    await contentManager.addContent(10, cid);
+
+    console.log(contentManager.contents)
+
+    await expect(contentManager.addContent(15, cid)).to.be.revertedWith('Content is already on the platform!');
+  });
+
+
+
+});
