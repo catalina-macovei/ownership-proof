a70f805cbbd0570c3ea18160e3811ef37ac658b7
Catalina Macovei <catalinamacovei@Catalinas-MacBook-Pro-2.local>
2025-01-03 12:58:25 +0200

    up hardhat configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,5 +8,9 @@ module.exports = {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY]
     }
-  }
+  },
+  paths: {
+    artifacts: "./frontend/src/artifacts",
+},
+
 };
